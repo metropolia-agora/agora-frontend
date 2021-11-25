67a2889e666c0767a8598f6ad23a2c12a7e55757
András Ádám <adm.andras@gmail.com>
2021-11-25 23:13:51 +0200

    Create project structure

---
+++ src/server.js
@@ -0,0 +1,50 @@
+// Import dependencies
+const express = require('express');
+const path = require('path');
+
+// Create express app
+const app = express();
+
+// Set port
+const port = 3000;
+
+// Set up static middleware
+app.use(express.static(path.join(__dirname, '')));
+
+// Home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/home.html'));
+});
+
+// Sign in page
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/signin.html'));
+});
+
+// Sign up page
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/signup.html'));
+});
+
+// Settings page
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/settings.html'));
+});
+
+// Profile page
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/profile.html'));
+});
+
+// View post page
+app.get('/post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/view-post.html'));
+});
+
+// New post page
+app.get('/new', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/new-post.html'));
+});
+
+// Start listening to requests
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
