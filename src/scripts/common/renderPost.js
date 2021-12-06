6c6ecab5f80e77ca7f916d0eb4e7940763f28291
András Ádám <adm.andras@gmail.com>
2021-12-06 20:09:15 +0200

    Add functionality to remove posts

--- src/scripts/common/renderPost.js
+++ src/scripts/common/renderPost.js
@@ -12,7 +12,9 @@ const icons = {
   },
 };
 
-const onReact = (postId, userId, type) => async () => {
+// Handle reacting to a post, update UI and call API
+const onPressReact = (postId, userId, type) => async (event) => {
+  event.stopImmediatePropagation();
   if (!userId) return;
   const upvoteCount = document.querySelector(`#post_${postId} > .post-actions > .upvote > p`);
   const downvoteCount = document.querySelector(`#post_${postId} > .post-actions > .downvote > p`);
@@ -52,24 +54,51 @@ const onReact = (postId, userId, type) => async () => {
       await api.post(`/api/posts/${postId}/reactions/${userId}`, { type });
     }
   }
+};
 
+// Handle removing a post, update UI and call API
+const onPressRemove = (postId) => async (event) => {
+  event.stopImmediatePropagation();
+  if (window.confirm('Are you sure you want to remove this post? This cannot be undone.')) {
+    await api.delete(`/api/posts/${postId}`);
+    const post = document.querySelector(`#post_${postId}`);
+    post.remove();
+  }
+};
+
+// Handle pressing the username and navigating to their profile page
+const onPressUser = (userId) => (event) => {
+  event.stopImmediatePropagation();
+  window.location.assign(`/profile?id=${userId}`);
+};
+
+// Handle pressing the post and navigating to the detailed post view
+const onPressPost = (postId) => (event) => {
+  event.stopImmediatePropagation();
+  window.location.assign(`/post?id=${postId}`);
+};
+
+// Handle pressing the comment button and navigating to the detailed post view's comment section
+const onPressComment = (postId) => (event) => {
+  event.stopImmediatePropagation();
+  window.location.assign(`/post?id=${postId}#comments`);
 };
 
 // Render post into parent element
 export const renderPost = (parent, post, user) => {
 
   // Container
-  const container = ElementHelper.create('div').setId(`post_${post.id}`).setClass('post-container');
+  const container = ElementHelper.create('div').setId(`post_${post.id}`).setOnClick(onPressPost(post.id)).setClass('post-container');
 
   // Header
   const header = ElementHelper.create('div').setClass('post-header').setParent(container);
-  const profilePicture = ElementHelper.create('div').setClass('post-header-profile').setParent(header);
+  const profilePicture = ElementHelper.create('div').setClass('post-header-profile').setOnClick(onPressUser(post.userId)).setParent(header);
   if (post.ownerFilename) {
     ElementHelper.create('img').setSrc(post.ownerFilename).setParent(profilePicture);
   } else {
     ElementHelper.create('img').setClass('icon').setSrc('assets/user.svg').setParent(profilePicture);
   }
-  ElementHelper.create('div').setClass('post-header-username').setText(post.ownerUsername).setParent(header);
+  ElementHelper.create('div').setClass('post-header-username').setText(post.ownerUsername).setOnClick(onPressUser(post.userId)).setParent(header);
   const formattedDate = moment(post.createdAt).fromNow();
   ElementHelper.create('div').setClass('post-header-timestamp').setText(formattedDate).setParent(header);
 
@@ -82,25 +111,25 @@ export const renderPost = (parent, post, user) => {
   const actions = ElementHelper.create('div').setClass('post-actions').setParent(container);
 
   // Actions - upvote
-  const upvote = ElementHelper.create('div').setClass('post-actions-item upvote').setOnClick(onReact(post.id, user?.id, 1)).setParent(actions);
+  const upvote = ElementHelper.create('div').setClass('post-actions-item upvote').setOnClick(onPressReact(post.id, user?.id, 1)).setParent(actions);
   const upvoteIcon = post.hasUpvoted ? 'assets/upvote_active.svg' : 'assets/upvote_inactive.svg';
   ElementHelper.create('img').setClass('icon').setSrc(upvoteIcon).setParent(upvote);
   ElementHelper.create('p').setText(post.upvoteCount).setParent(upvote);
 
   // Actions - downvote
-  const downvote = ElementHelper.create('div').setClass('post-actions-item downvote').setOnClick(onReact(post.id, user?.id, -1)).setParent(actions);
+  const downvote = ElementHelper.create('div').setClass('post-actions-item downvote').setOnClick(onPressReact(post.id, user?.id, -1)).setParent(actions);
   const downvoteIcon = post.hasDownvoted ? 'assets/downvote_active.svg' : 'assets/downvote_inactive.svg';
   ElementHelper.create('img').setClass('icon').setSrc(downvoteIcon).setParent(downvote);
   ElementHelper.create('p').setText(post.downvoteCount).setParent(downvote);
 
   // Actions - comment
-  const comment = ElementHelper.create('div').setClass('post-actions-item comment').setParent(actions);
+  const comment = ElementHelper.create('div').setClass('post-actions-item comment').setOnClick(onPressComment(post.id)).setParent(actions);
   ElementHelper.create('img').setClass('icon').setSrc('assets/comment.svg').setParent(comment);
   ElementHelper.create('p').setText(post.commentCount).setParent(comment);
 
   // Actions - remove
-  if (user.type === 2) {
-    const remove = ElementHelper.create('div').setClass('post-actions-item remove').setParent(actions);
+  if (user.type === 2 || user.id === post.userId) {
+    const remove = ElementHelper.create('div').setClass('post-actions-item remove').setOnClick(onPressRemove(post.id)).setParent(actions);
     ElementHelper.create('img').setClass('icon').setSrc('assets/remove.svg').setParent(remove);
   }
 
