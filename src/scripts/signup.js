4101b0b239e25b66bfa4a832962e05db58883ef3
Sirja Kosonen <sirja.kosonen@gmail.com>
2021-12-08 09:46:30 +0200

    Add sign up elements and functionality

--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -1,5 +1,43 @@
 import { authentication } from './common/authentication';
+import { api } from "./common/api";
 
 // Authentication check
 const user = await authentication.check();
 console.log('Signed in as', user || 'anon');
+
+// Pick html elements
+const form = document.getElementById('signup-form');
+const inputUsername = document.getElementById('field-username');
+const inputPassword = document.getElementById('field-password');
+const inputPasswordMatch = document.getElementById('field-passwordMatch');
+
+// Read values from input fields and check if passwords match when button clicked
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = inputUsername.value;
+    const password = inputPassword.value;
+    const doesMatch = inputPasswordMatch.value;
+
+    // Make an api call if passwords match
+    // If not, show an error
+    if (password === doesMatch) {
+        const response = await api.post('/api/users', { username, password });
+
+        // If username already exists, show an error
+        // If all is good, store credentials to localStorage and redirect to home page
+        if (!response.ok) {
+            alert(response.details.username);
+            inputUsername.value = '';
+        } else {
+            const { user, token } = response;
+            authentication.signin(token, user.id);
+        }
+
+    } else {
+        alert('Passwords do not match.');
+        inputPassword.value = '';
+        inputPasswordMatch.value = '';
+    }
+
+});
