a98c33839a7538568f3a50e54d107add70fa6d48
András Ádám <adm.andras@gmail.com>
2021-12-09 19:06:40 +0200

    Install eslint

--- src/scripts/signin.js
+++ src/scripts/signin.js
@@ -1,12 +1,12 @@
 import { authentication } from './common/authentication';
-import { api } from "./common/api";
+import { api } from './common/api';
 
 // Authentication check
 const user = await authentication.check();
 console.log('Signed in as', user || 'anon');
 
 if (user) {
-    window.location.assign('/');
+  window.location.assign('/');
 }
 
 // Pick html elements
@@ -16,23 +16,23 @@ const inputPassword = document.getElementById('field-password');
 
 // Read values from input fields and call api when button clicked
 form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = inputUsername.value;
-    const password = inputPassword.value;
+  const username = inputUsername.value;
+  const password = inputPassword.value;
 
-    // Check if the given credentials are valid.
-    const response = await api.post('/api/users/auth', { username, password });
+  // Check if the given credentials are valid.
+  const response = await api.post('/api/users/auth', { username, password });
 
-    // If given credentials are not valid, display en error
-    // If all is good, store credentials to localStorage and redirect to home page
-    if (!response.ok) {
-        alert(response.message);
-        inputUsername.value = '';
-        inputPassword.value = '';
-    } else {
-        const {user, token} = response;
-        authentication.signin(token, user.id);
-    }
+  // If given credentials are not valid, display en error
+  // If all is good, store credentials to localStorage and redirect to home page
+  if (!response.ok) {
+    alert(response.message);
+    inputUsername.value = '';
+    inputPassword.value = '';
+  } else {
+    const { user, token } = response;
+    authentication.signin(token, user.id);
+  }
 
 });
--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -1,12 +1,12 @@
 import { authentication } from './common/authentication';
-import { api } from "./common/api";
+import { api } from './common/api';
 
 // Authentication check
 const user = await authentication.check();
 console.log('Signed in as', user || 'anon');
 
 if (user) {
-    window.location.assign('/');
+  window.location.assign('/');
 }
 
 // Pick html elements
@@ -17,31 +17,31 @@ const inputPasswordMatch = document.getElementById('field-passwordMatch');
 
 // Read values from input fields and check if passwords match when button clicked
 form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = inputUsername.value;
-    const password = inputPassword.value;
-    const doesMatch = inputPasswordMatch.value;
+  const username = inputUsername.value;
+  const password = inputPassword.value;
+  const doesMatch = inputPasswordMatch.value;
 
-    // Make an api call if passwords match
-    // If not, show an error
-    if (password === doesMatch) {
-        const response = await api.post('/api/users', { username, password });
+  // Make an api call if passwords match
+  // If not, show an error
+  if (password === doesMatch) {
+    const response = await api.post('/api/users', { username, password });
 
-        // If username already exists, show an error
-        // If all is good, store credentials to localStorage and redirect to home page
-        if (!response.ok) {
-            alert(response.details.username);
-            inputUsername.value = '';
-        } else {
-            const { user, token } = response;
-            authentication.signin(token, user.id);
-        }
-
+    // If username already exists, show an error
+    // If all is good, store credentials to localStorage and redirect to home page
+    if (!response.ok) {
+      alert(response.details.username);
+      inputUsername.value = '';
     } else {
-        alert('Passwords do not match.');
-        inputPassword.value = '';
-        inputPasswordMatch.value = '';
+      const { user, token } = response;
+      authentication.signin(token, user.id);
     }
+
+  } else {
+    alert('Passwords do not match.');
+    inputPassword.value = '';
+    inputPasswordMatch.value = '';
+  }
 
 });
