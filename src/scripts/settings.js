0c544efe00ee7f79872099ab82cd75ec00478cfc
Sirja Kosonen <sirja.kosonen@gmail.com>
2021-12-09 14:57:41 +0200

    Add changing username and password functionality

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,5 +1,37 @@
 import { authentication } from './common/authentication';
+import { api } from "./common/api";
 
 // Authentication check
 const user = await authentication.check();
 console.log('Signed in as', user || 'anon');
+
+/*if (!user) {
+    window.location.assign('/');
+}*/
+
+// Select html elements
+const formChangeUsername = document.getElementById('change-username');
+const formChangePassword = document.getElementById('change-password');
+const formDeleteUser = document.getElementById('delete-user');
+const inputUsername = document.getElementById('field-username');
+const inputCurrentPassword = document.getElementById('field-currentPassword');
+const inputNewPassword = document.getElementById('field-newPassword');
+const inputNewPasswordMatch = document.getElementById('field-newPasswordMatch');
+const inputDeleteUser = document.getElementById('field-password');
+const logoutButton = document.getElementById('logout');
+const successNotification = document.getElementById('success');
+
+// CHANGING USERNAME
+formChangeUsername.addEventListener('submit', async (event) => {
+   event.preventDefault();
+
+   const username = inputUsername.value;
+   const response = await api.post(`/api/users/${user.id}/username`, { username });
+
+   if (!response.ok) {
+      alert(response.message);
+      inputUsername.value = '';
+   } else {
+   }
+});
+
