3ba2f8e0e71b853ff067a4d1dcc0baf5bcb9c3a3
András Ádám <adm.andras@gmail.com>
2021-12-14 22:11:19 +0200

    Add profile picture upload

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -15,18 +15,61 @@ const navbar = document.querySelector('#navigation-bar');
 renderNavigationBar(navbar, currentUser);
 
 // Select html elements
+const formChangePicture = document.getElementById('change-picture');
 const formChangeUsername = document.getElementById('change-username');
 const formChangePassword = document.getElementById('change-password');
 const formDeleteUser = document.getElementById('delete-user');
+const inputPicture = document.getElementById('field-picture');
 const inputUsername = document.getElementById('field-username');
 const inputCurrentPassword = document.getElementById('field-currentPassword');
 const inputNewPassword = document.getElementById('field-newPassword');
 const inputNewPasswordMatch = document.getElementById('field-newPasswordMatch');
 const inputDeleteUser = document.getElementById('field-password');
+const buttonSubmitPicture = document.getElementById('submit-picture');
 const successNotification = document.getElementById('saved');
 
 // Pre-fill username input with current username
 inputUsername.value = currentUser.username;
+
+// Success helper
+const showSuccessMessage = () => {
+  successNotification.classList.add('visible');
+  setTimeout( () => {
+    successNotification.classList.remove('visible');
+  }, 3000);
+};
+
+// Set save changes button text for profile picture upload
+inputPicture.addEventListener('change', () => {
+  buttonSubmitPicture.textContent = inputPicture.files.length ? 'Update picture' : 'Remove picture';
+});
+
+// CHANGE PROFILE PICTURE
+formChangePicture.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  // Create form-data body
+  const body = new FormData();
+  if (inputPicture.files.length > 0) body.append('file', inputPicture.files[0]);
+
+  // Check file size
+  if (body.has('file')) {
+    const file = body.get('file');
+    if (file.size >= 10485760) {
+      window.alert('The file is too large. Maximum 10MB.');
+      return;
+    }
+  }
+
+  // Send the file (or empty body) to the api
+  const response = await api.post(`/api/users/${currentUser.id}/picture`, body);
+  if (!response.ok) {
+    window.alert(response.message);
+  } else {
+    showSuccessMessage();
+  }
+
+});
 
 // CHANGE USERNAME
 formChangeUsername.addEventListener('submit', async (event) => {
@@ -43,10 +86,7 @@ formChangeUsername.addEventListener('submit', async (event) => {
   if (!response.ok) {
     alert(response.message);
   } else {
-    successNotification.classList.add('visible');
-    setTimeout( () => {
-      successNotification.classList.remove('visible');
-    }, 3000);
+    showSuccessMessage();
   }
 });
 
@@ -70,13 +110,10 @@ formChangePassword.addEventListener('submit', async (event) => {
     if (!response.ok) {
       alert(response.message);
     } else {
-      successNotification.classList.add('visible');
-      setTimeout( () => {
-        successNotification.classList.remove('visible');
-      }, 3000);
       inputCurrentPassword.value = '';
       inputNewPassword.value = '';
       inputNewPasswordMatch.value = '';
+      showSuccessMessage();
     }
   } else {
     alert('Passwords do not match.');
