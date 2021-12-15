ae111bb198b062969bf50edbefcfcb32d6dd58b3
Sirja Kosonen <sirja.kosonen@gmail.com>
2021-12-15 18:26:49 +0200

    Fixes

--- src/scripts/common/renderTopMenu.js
+++ src/scripts/common/renderTopMenu.js
@@ -8,44 +8,43 @@ const onPressLogout = () => (event) => {
 }
 
 export const renderTopMenu = (user) => {
-    // Pick top menu elements
-    const hamburger = document.getElementById('menuButton');
-    const ul = document.querySelector('.menubar');
-    const liProfile = document.createElement('li');
-    const liSettings = document.createElement('li');
-    const liLogout = document.createElement('li');
-    const liSignin = document.createElement('li');
-    const liSignup = document.createElement('li');
+
+    // Pick and create main top menu elements
+    const navbar = document.getElementById('top-menu');
+    const ul = ElementHelper.create('ul').setClass('menubar hidden').setParent(navbar);
+    const liHome = ElementHelper.create('li').setParent(ul);
+    ElementHelper.create('a').setHref('/').setText('Home').setParent(liHome);
 
     // Toggle hamburger menu on and off (hidden, not hidden) when button clicked.
     // When menu is shown, prevent scrolling.
-    hamburger.addEventListener('click', (event) => {
+    const onPressMenu = () => (event) => {
         event.preventDefault();
-        ul.classList.toggle('hidden');
+        ul.htmlElement.classList.remove('hidden');
+        document.body.style.overflowY = 'hidden';
 
-        if (!ul.classList.contains('hidden')) {
-            document.body.style.overflowY = 'hidden';
-        } else {
-            document.body.style.overflowY = '';
-        }
-    });
+        // TODO: add close button to ul
+    }
+
+    // Create the last main top menu element (actual button and it's icon)
+    const menuButton = ElementHelper.create('button').setId('menuButton').setOnClick(onPressMenu()).setParent(navbar);
+    ElementHelper.create('i').setClass('fas fa-bars').setParent(menuButton);
 
     // If user if logged in, show buttons for profile, settings and logout.
     // If not, show buttons for sign in and sign up.
     if (user) {
-        ul.appendChild(liProfile);
-        ul.appendChild(liSettings);
-        ul.appendChild(liLogout);
-        liProfile.appendChild(ElementHelper.create('a').setHref(`/profile?id=${user.id}`).setText('Profile').htmlElement);
-        liSettings.appendChild(ElementHelper.create('a').setHref('/settings').setText('Settings').htmlElement);
-        liLogout.appendChild(ElementHelper.create('button').setClass('button').setId('logout').setOnClick(onPressLogout()).htmlElement);
+        const liProfile = ElementHelper.create('li').setParent(ul);
+        const liSettings = ElementHelper.create('li').setParent(ul);
+        const liLogout = ElementHelper.create('li').setParent(ul);
+        ElementHelper.create('a').setHref(`/profile?id=${user.id}`).setText('Profile').setParent(liProfile);
+        ElementHelper.create('a').setHref('/settings').setText('Settings').setParent(liSettings);
+        ElementHelper.create('button').setClass('button').setId('logout').setOnClick(onPressLogout()).setParent(liLogout);
 
         const button = document.getElementById('logout');
-        button.appendChild(ElementHelper.create('i').setClass('fas fa-sign-out-alt').htmlElement);
+        ElementHelper.create('i').setClass('fas fa-sign-out-alt').setParent(button)
     } else {
-        ul.appendChild(liSignin);
-        ul.appendChild(liSignup);
-        liSignin.appendChild(ElementHelper.create('a').setHref('/signin').setText('Sign in').htmlElement);
-        liSignup.appendChild(ElementHelper.create('a').setHref('/signout').setText('Sign up').htmlElement);
+        const liSignin = ElementHelper.create('li').setParent(ul);
+        const liSignup = ElementHelper.create('li').setParent(ul);
+        ElementHelper.create('a').setHref('/signin').setText('Sign in').setParent(liSignin);
+        ElementHelper.create('a').setHref('/signout').setText('Sign out').setParent(liSignup);
     }
 };
